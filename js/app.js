4f765ef7cafb00856168d61c9f70747b90da62cd
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-09-14 16:15:11 +0200

    Add winner-looser section

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            roundCounter: 0
+            roundCounter: 0,
+            winner: null
         }
     },
     methods: {
@@ -26,17 +27,51 @@ const app = Vue.createApp({
             this.monsterHealth -= attackValue;
             this.attackToPlayer();
             this.roundCounter++;
+        },
+        healPlayer() {
+            const healValue = getRandomValue(8, 15);
+            this.playerHealth += healValue;
         }
     },
     computed: {
         monsterBarStyles() {
-            return{
+            if (this.monsterHealth <= 0) {
+               return {
+                width: '0%'
+               }
+            }  
+            return {
                 width: this.monsterHealth + '%'
             }
         },
         playerBarStyles() {
-            return{
+            if (this.playerHealth <= 0) {
+                return {
+                    width: '0%'
+                }
+            } 
+            return {
                 width: this.playerHealth + '%'
+            }
+        },
+        mayUseSpecialAttack() {
+            return this.roundCounter % 3 !== 0
+        }
+
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                // draw
+            } else if (value <= 0) {
+                // player lost
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <=0) {
+                //draw
+            } else if (value <= 0) {
+                //monster lost
             }
         }
 
