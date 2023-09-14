1f191613e50e9a1948aa0b8e7dec6e600c20075f
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-09-14 10:19:27 +0200

    Add specialAttackToMonster()

--- js/app.js
+++ js/app.js
@@ -1,23 +1,45 @@
 function getRandomValue(min, max) {
-    return Math.floor(Math.random(max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
     data(){
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            roundCounter: 0
         }
     },
     methods: {
-        attackMonster() {
+        attackToMonster() {
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
-            this.attackPlayer();
+            this.attackToPlayer();
+            this.roundCounter++;
         },
-        attackPlayer() {
+        attackToPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackToMonster() {
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackToPlayer();
+            this.roundCounter++;
         }
+    },
+    computed: {
+        monsterBarStyles() {
+            return{
+                width: this.monsterHealth + '%'
+            }
+        },
+        playerBarStyles() {
+            return{
+                width: this.playerHealth + '%'
+            }
+        }
+
     }
-})
+});
+app.mount('#game');
